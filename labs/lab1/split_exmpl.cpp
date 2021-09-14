171b015f4659562becde4e87f6d5323175f35ea1
Alexander Orlov <65221009+alxorlov@users.noreply.github.com>
2021-09-14 12:25:43 +0300

    Update split_exmpl.cpp

--- labs/lab1/split_exmpl.cpp
+++ labs/lab1/split_exmpl.cpp
@@ -1,16 +1,19 @@
 #include <iostream>
+#include <string>
 #include <vector>
 
 std::vector<std::string> split(const std::string& str, char delim)
 {
     std::vector<std::string> tokens;
 
-    size_t start = 0, end = str.find(delim, start);
-    while (end != std::string::npos)
+    if (!str.empty())
     {
-        tokens.push_back(str.substr(start, (end - start)));
-        start = end + 1;
-        end = str.find(delim, start);
+        size_t start = 0, end;
+        do {
+            tokens.push_back(str.substr(start, (end - start)));
+            end = str.find(delim, start);
+            start = end + 1;
+        } while (end != std::string::npos);
     }
 
     return tokens;
@@ -21,7 +24,7 @@ int main()
     std::string mystr = "String delimited with spaces";
 
     std::cout << "[ ";
-    for (auto s : split(mystr, ' '))
+    for (const auto &s : split(mystr, ' '))
         std::cout << s << ", ";
     std::cout << " ]" << std::endl;
 
